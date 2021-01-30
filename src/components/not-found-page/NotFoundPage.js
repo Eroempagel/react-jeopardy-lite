159652a9000a82d91d1d5a120ab9070e38fda646
Erik <eroempagel@outlook.com>
2021-01-30 16:17:39 -0600

    added in starter code from the routing app

---
+++ src/components/not-found-page/NotFoundPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class NotFoundPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          <div className="not-found-page">404</div> Page not found!
+        </h2>
+        <p>We are sorry, but the page you are looking for does not exist.</p>
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
